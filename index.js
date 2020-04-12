92d0bd81421cd689785a7ac7ea96508233a24958
Andres Ruiz-Velediaz <aruiz858@gmail.com>
2020-04-11 23:43:49 -0700

    Given credentials and a valid Policy# w/ information will resultant in success.

--- index.js
+++ index.js
@@ -3,6 +3,12 @@ const CREDS = require('./creds');
 const USERNAME = '#UserID';
 const PASSWORD = '#UserPW';
 const LOGIN = '#cmdSubmit';
+const POLICY = '#RecipID';
+const DOB = '#RecipDOB';
+const DOS = '#RecipDOS';
+const DOI = '#RecipDOI';
+const ISSUE_DATE = new Date().toLocaleDateString();
+const SUBMIT = '#middle_column > div.column_inner > form > div:nth-child(2) > input[type=submit]:nth-child(1)';
 const URL = 'https://www.medi-cal.ca.gov/Eligibility/Login.asp';
 
 async function run() {
@@ -14,14 +20,23 @@ async function run() {
     console.log("@Login page");
     await page.click(USERNAME);
     await page.keyboard.type(CREDS.username);
-
     await page.click(PASSWORD);
     await page.keyboard.type(CREDS.password);
-    await page.screenshot({ path: 'screenshots/medical.png' });
     await page.click(LOGIN);
     await page.goto("https://www.medi-cal.ca.gov/Eligibility/Eligibility.asp", { waitUntil: 'networkidle2' });
     console.log("@Eligibility Page");
-    browser.close();
+    await page.click(POLICY);
+    await page.keyboard.type(CREDS.policy);
+    await page.click(DOB);
+    await page.keyboard.type(CREDS.dob);
+    await page.click(DOS);
+    await page.keyboard.type(CREDS.dos);
+    await page.click(DOI);
+    await page.keyboard.type(ISSUE_DATE);
+    await page.click(SUBMIT);
+    await page.waitForNavigation();
+    console.log("@Resultant Page");
+    // browser.close();
 }
 
 run();
