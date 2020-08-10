d832edc927d5bc900051cdf6cace88e2d068ef02
Andres Ruiz-Velediaz <aruiz858@gmail.com>
2020-08-10 11:57:24 -0700

    Update for PTN process

--- index.js
+++ index.js
@@ -1,15 +1,23 @@
 const puppeteer = require('puppeteer');
-const CREDS = require('./creds');
-const USERNAME = '#UserID';
-const PASSWORD = '#UserPW';
-const LOGIN = '#cmdSubmit';
+const CREDS = require('./creds.js');
+const USERNAME = '#MainContent_txtUserID';
+const PASSWORD = '#MainContent_txtPassword';
+const LOGIN = '#MainContent_btnSubmit';
+
+//Eligiblity
 const POLICY = '#RecipID';
 const DOB = '#RecipDOB';
 const DOS = '#RecipDOS';
 const DOI = '#RecipDOI';
+
+//Claim Status Transcation (PTN)
+const CST_ID = '#MainContent_txtSubscriberID';
+const CST_DOS = '#MainContent_txtDOSFrom';
+
+
 const ISSUE_DATE = new Date().toLocaleDateString();
 const SUBMIT = '#middle_column > div.column_inner > form > div:nth-child(2) > input[type=submit]:nth-child(1)';
-const URL = 'https://www.medi-cal.ca.gov/Eligibility/Login.asp';
+const URL = 'https://www.medi-cal.ca.gov/MCWebPub/Login.aspx';
 
 async function run() {
     const browser = await puppeteer.launch({ headless: true });
@@ -23,21 +31,43 @@ async function run() {
     await page.click(PASSWORD);
     await page.keyboard.type(CREDS.password);
     await page.click(LOGIN);
-    await page.goto("https://www.medi-cal.ca.gov/Eligibility/Eligibility.asp", { waitUntil: 'networkidle2' });
-    console.log("@Eligibility Page");
-    await page.click(POLICY);
-    await page.keyboard.type(CREDS.policy);
-    await page.click(DOB);
-    await page.keyboard.type(CREDS.dob);
-    await page.click(DOS);
-    await page.keyboard.type(CREDS.dos);
-    await page.click(DOI);
-    await page.keyboard.type(ISSUE_DATE);
-    await page.click(SUBMIT);
-    await page.waitForNavigation({ timeout: 1000, waitUntil: 'load' }).catch(err => console.log(err));
-    console.log("@Resultant Page");
-    const data = await page.$$eval('center > b', info => info.map(item => item.innerHTML));
-    console.log(data);
-    browser.close();
+    var result = [];
+
+    for (var i = 0; i < 100; i++) {
+        await page.goto("https://www.medi-cal.ca.gov/APS/ClaimStatus.aspx", { waitUntil: 'networkidle2' });
+        console.log("@Eligibility Page");
+        var temp_DOS = CREDS.cst_DOS[i];
+        if (CREDS.cst_DOS[i] = "" || CREDS.cst_ID[i] == "") {
+            result[i] = "NOT IN MEDICAL";
+        } else {
+            console.log(temp_DOS);
+            //#region Claim Status Transaction Process
+            await page.click(CST_ID);
+            await page.keyboard.type(CREDS.cst_ID[i]);
+            await page.click(CST_DOS);
+            await page.type(CST_DOS, temp_DOS);
+            await page.click(LOGIN);
+            await page.waitForNavigation({ timeout: 4000, waitUntil: 'load' }).catch(err => console.log(err));
+            console.log("@Resultant Page");
+            const EFT = await page.$$eval('#MainContent_dvOut > center > table > tbody > tr:nth-child(5) > td:nth-child(4) > b', info => info.map(item => item.innerHTML));
+            const DATE = await page.$$eval('#MainContent_dvOut > center > table > tbody > tr:nth-child(5) > td:nth-child(2) > b', info => info.map(item => item.innerHTML));
+            result[i] = `${EFT}, ${DATE}`;
+        }
+    }
+    //#endregion
+
+    //#region Eligiblity Process 
+    // await page.click(POLICY);
+    // await page.keyboard.type(CREDS.policy);
+    // await page.click(DOB);
+    // await page.keyboard.type(CREDS.dob);
+    // await page.click(DOS);
+    // await page.keyboard.type(CREDS.dos);
+    // await page.click(DOI);
+    // await page.keyboard.type(ISSUE_DATE);
+    // await page.click(SUBMIT);
+    //#endregion
+    console.log(result);
+    // browser.close();
 }
 run();
